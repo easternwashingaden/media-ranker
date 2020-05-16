6584f9ffe9344f4585be6d179184a71a20186621
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-16 00:23:00 -0700

    Created the index view and show the list of works based on each category

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,100 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  # def create
+  #   @driver = Driver.new(driver_params)
+  #   if @driver.save
+  #     # flash[:success] = "Driver added successfully"
+  #     redirect_to driver_path(@driver)
+  #     return
+  #   else
+  #     # flash.now[:error] = "Something went wrong. Driver not added"
+  #     render :new, status: :bad_request
+  #     return
+  #   end
+  # end
+
+  # def edit
+  #   @driver = Driver.find_by(id: params[:id])
+    
+  #   if @driver.nil?
+  #     redirect_to drivers_path
+  #     return
+  #   end
+  # end
+
+  # def update
+  #   @driver = Driver.find_by(id: params[:id])
+    
+  #   if @driver.nil?
+  #     head :not_found
+  #     return
+  #   elsif @driver.update(driver_params)
+  #     redirect_to driver_path(@driver)
+  #     return
+  #   else 
+  #     render :edit, status: :bad_request
+  #     return
+  #   end
+  # end
+  
+  # def destroy
+  #   @driver = Driver.find_by(id: params[:id])
+    
+  #   if @driver.nil?
+  #     head :not_found
+  #     return
+  #   elsif Trip.where(driver_id: @driver.id).count > 0 # if the driver has trips, we want to destroy them first; otherwise, the delete driver button will cause an error
+  #     Trip.where(driver_id: @driver.id).destroy_all
+  #     @driver.destroy
+  #     redirect_to drivers_path
+  #     return
+  #   else
+  #     @driver.destroy
+  #     redirect_to drivers_path
+  #     return
+  #   end
+  # end 
+
+  # def toggle_available
+  #   @driver = Driver.find_by(id: params[:id])
+
+  #   if @driver.nil?
+  #     head :not_found
+  #     return
+  #   end
+    
+  #   # Update the driver's availability
+  #   if @driver.available == false
+  #     @driver.update(available: true)
+  #     redirect_to driver_path
+  #     return
+  #   else 
+  #     @driver.update(available: false)
+  #     redirect_to driver_path
+  #     return
+  #   end   
+  # end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/create'
-  get 'homepages/index'
+  
+  root to: "homepages#index"
+
+  resources :works
+  
+#   resources :passengers do
+#     resources :trips, only: [:create]
+#   end
+  
+#   resources :trips, except: [:index, :new, :create]
+#   patch '/trips/:id/complete_trip', to: 'trips#complete_trip', as: 'complete_trip'
+# end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
