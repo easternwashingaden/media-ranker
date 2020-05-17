532d219435c3b22440140ffc47013cd60bb4ae49
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-16 22:16:41 -0700

    Added validations for work form and also created self.top_ten_list and self.spot_light

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,20 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+
+  validates :category, presence: true
+  validates :title, uniqueness: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+  validates :description, presence: true
+
+  def self.top_ten_list(category)
+    works = Work.where(category: category)
+    return works.limit(10)
+  end
+
+  def self.spot_light
+    works = Work.all
+    return works.sample
+  end
 end
