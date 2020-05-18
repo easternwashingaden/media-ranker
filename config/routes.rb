3e4d27e18c294f588d502a668c4b752d25aaa6b6
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-18 02:42:56 -0700

    Added the login and logout routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+  
   root to: "homepages#index"
 
   resources :works
-  
-#   resources :passengers do
-#     resources :trips, only: [:create]
-#   end
-  
-#   resources :trips, except: [:index, :new, :create]
-#   patch '/trips/:id/complete_trip', to: 'trips#complete_trip', as: 'complete_trip'
-# end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
