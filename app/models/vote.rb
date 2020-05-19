a5f79bff036313a947e069e4411b58fea13c6f5e
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-19 09:58:46 -0700

    Added some validation for Vote and count_votes function

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,21 @@
 class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
+
+  validates :user_id, :work_id, presence: true
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
+
+
+  def work
+    return Work.find_by(id: self.work_id)
+  end
+
+  def self.count_votes(work_id)
+    return Vote.where(work_id: work_id).count 
+  end
+
+  
 end
