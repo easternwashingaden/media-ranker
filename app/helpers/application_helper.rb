3a51a9855ecd5dd823724754e8049816e4372ded
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-22 04:33:36 -0700

    Added the flash_class helper method for the flash classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-notice"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-error"
+      when 'warning' then "alert alert-warning"
+    end
+  end
 end
