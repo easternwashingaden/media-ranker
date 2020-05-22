95ecf7ff4753e286b31033bb2fee876b0e5f7ec1
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-22 04:40:23 -0700

    Added some validation and custom tests for vote model

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,63 +1,75 @@
-# require "test_helper"
+require "test_helper"
 
-# describe Vote do
+describe Vote do
+  before do
+    @new_work = Work.new(
+      category: "album", 
+      title: "My Coding Journey",
+      creator: "Lak Mok",
+      publication_year: "2020",
+      description: "Ada makes my dream come true"
+    )
+    @new_vote = Vote.new(
+      user: users(:user3),
+      work: works(:album8)
+    )
+  end
 
-#   let (:new_passenger) {
-#     Passenger.new(name: "Kari", phone_num: "111-111-1211")
-#   }
+  it "can be instantiated" do
+    expect(@new_vote.valid?).must_equal true
+  end
 
-#   it "can be instantiated" do
-#     # Assert
-#     expect(new_passenger.valid?).must_equal true
-#   end
+  it "will have the required fields" do
+    @new_vote.save
+    vote= Vote.first
+    [:user_id, :work_id].each do |field|
+      # Assert
+      expect(vote).must_respond_to field
+    end
+  end
 
-#   it "will have the required fields" do
-#     # Arrange
-#     new_passenger.save
-#     passenger = Passenger.first
-#     [:name, :phone_num].each do |field|
-
-#       # Assert
-#       expect(passenger).must_respond_to field
-#     end
-#   end
-
-#   describe "relationships" do
-#     it "can have many trips" do
-#       # Arrange
-#       new_passenger.save
-#       new_driver = Driver.create!(name: "Waldo", vin: "ALWSS52P9NEYLVDE9", available: true)
-#       trip_1 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
-#       trip_2 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
+  describe "relationships" do
+    it "vote can link to only one user and work" do
+      # Arrange
+      @new_vote.save
       
-#       # Assert
-#       expect(new_passenger.trips.count).must_equal 2
-#       new_passenger.trips.each do |trip|
-#         expect(trip).must_be_instance_of Trip
-#       end
-#     end
-#   end
+      expect(@new_vote.user).must_be_instance_of User
+    
+      expect(@new_vote.work).must_be_instance_of Work
+    
+    end
+  end
 
-#   describe "validations" do
-#     it "must have a name" do
-#       # Arrange
-#       new_passenger.name = nil
+  describe "validations" do
+    it "must have a work" do
+      # Arrange
+      @new_vote.work = nil
 
-#       # Assert
-#       expect(new_passenger.valid?).must_equal false
-#       expect(new_passenger.errors.messages).must_include :name
-#       expect(new_passenger.errors.messages[:name]).must_equal ["can't be blank"]
-#     end
+      # Assert
+      expect(@new_vote.valid?).must_equal false
+      expect(@new_vote.errors.messages).must_include :work
+      expect(@new_vote.errors.messages[:work_id]).must_equal ["can't be blank"]
+    end
 
-#     it "must have a phone number" do
-#       # Arrange
-#       new_passenger.phone_num = nil
+    it "must have a user" do
+      # Arrange
+      @new_vote.user = nil
 
-#       # Assert
-#       expect(new_passenger.valid?).must_equal false
-#       expect(new_passenger.errors.messages).must_include :phone_num
-#       expect(new_passenger.errors.messages[:phone_num]).must_equal ["can't be blank"]
-#     end
-#   end
-  
-# end
+      # Assert
+      expect(@new_vote.valid?).must_equal false
+      expect(@new_vote.errors.messages).must_include :user
+      expect(@new_vote.errors.messages[:user_id]).must_equal ["can't be blank"]
+    end
+  end
+
+  # Test custom methods
+
+  describe "count_votes" do
+
+    it "returns the count of votes of a particular " do
+      album5 = works(:album5)
+      # From the fixture, there are 4 votes for album 5
+      expect(Vote.count_votes(album5.id)).must_equal 4
+    end
+  end
+end
