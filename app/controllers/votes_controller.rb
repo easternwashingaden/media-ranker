48248a4e282a3497ecba35ba5121733488d60d57
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-22 04:32:57 -0700

    Updated the flash message to match the original work

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,34 +1,33 @@
 class VotesController < ApplicationController
+  def new
+    @vote = Vote.new
+  end
   
   def create
     # Check if there is a user logged in
     if session[:user_id]
       @user = User.find_by(id: session[:user_id])
       @work = Work.find_by(id: params[:work_id])
-      user_voted_works_list = @user.votes.map {|vote| vote.work_id }
-      
-      if user_voted_works_list.include?(@work.id)
-        flash[:warning] = "user: has already voted for this work"
+    
+      @vote = Vote.new(
+        user_id: @user.id,
+        work_id: @work.id
+      )
+      if @vote.save
+        flash[:success] = "Successfully upvoted"
         redirect_to works_path
+        return
       else
-        @vote = Vote.new(
-          user_id: @user.id,
-          work_id: @work.id
-        )
-        if @vote.save
-          flash[:success] = "Successfully upvoted"
-          redirect_to works_path
-          return
-        end
+        flash[:warning] = "A problem occurred: Could not upvote"
+        redirect_to works_path
+        return
       end
     else
-      flash[:error] = "You have to log in order to upvote"
+      flash[:error] = "A problem occurred: You must log in to do that"
       redirect_to works_path
       return
     end
   end
-
-  
 
   private
   def vote_params
