ecdd78add8fc353a077b229c099c84186b860fb1
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-22 04:32:30 -0700

    Changed the flash message to match the original work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,32 +18,32 @@ class UsersController < ApplicationController
   end
 
   def login
-    user = User.find_by(name: params[:user][:name])
+    @user = User.find_by(name: params[:user][:name])
 
-    if user.nil?
+    if @user.nil?
       # New user
-      user = User.new(name: params[:user][:name])
-      if ! user.save
-          flash[:error] = "Unable to login"
-          redirect_to root_path
+      @user = User.new(name: params[:user][:name])
+      if ! @user.save
+          flash.now[:error] = "A problem occurred: Could not log in"
+          render :login_form, status: :bad_request
           return
       end
-      flash[:welcome] = "Welcome #{user.name}"
+      flash[:success] = "Successfully created new user #{@user.name} with ID #{@user.id}"
     else
       # Existing User
-      flash[:welcome] = "Welcome back #{user.name}"
+      flash[:success] = "Successfully logged in as existing user #{@user.name}"
     end
 
-    session[:user_id] = user.id 
+    session[:user_id] = @user.id 
     redirect_to root_path
   end
 
   def logout 
     if session[:user_id]
-      user = User.find_by(id: session[:user_id])
-      unless user.nil?
+      @user = User.find_by(id: session[:user_id])
+      unless @user.nil?
         session[:user_id] = nil
-        flash[:notice] = "Goodby #{user.name}"
+        flash[:success] = "Successfully logged out. Goodbye #{@user.name}"
       else
         session[:user_id] = nil
         flash[:notice] = "Error Unknown User"
@@ -54,15 +54,16 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
-  def current
-    @user = User.find_by(id: session[:user_id])
+  # The current method is not needed for this project but leave it here FFY
+  # def current
+  #   @user = User.find_by(id: session[:user_id])
 
-    if @user.nil?
-      flash[:error] = "You must be logged in to view this page"
-      redirect_to root_path
-      return
-    end
-  end
+  #   if @user.nil?
+  #     flash[:error] = "You must be logged in to view this page"
+  #     redirect_to root_path
+  #     return
+  #   end
+  # end
 
   private
   def user_params
